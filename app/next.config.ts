de8e7bab47bb33936b2b08f96779e4d767bb7785
indianapoly <hyeonlimgo5@gmail.com>
2025-07-27 14:12:42 +0900

    fix not work gh pages

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: "https://indianapoly.github.io/react-animation-typing-text/"
 };
 
 export default nextConfig;
